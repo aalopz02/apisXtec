df2ad1479f08478bed7718c87e22abc8e6101575
aalopz02 <aalopz02@gmail.com>
2020-11-25 23:12:59 -0600

    se hace post y getAll

--- ApiMongo/Controllers/EstudianteController.cs
+++ ApiMongo/Controllers/EstudianteController.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using ApiMongo.Mist;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -12,20 +13,22 @@ namespace ApiMongo.Controllers
     public class EstudianteController : ApiController
     {
         // GET: Estudiante
-        public List<BsonDocument> Get()
+        public ICollection<BsonDocument> Get()
         {
-            try
-            {
-                var connString = "mongodb://localhost:27017";
-                MongoClient client = new MongoClient(connString);
-                var allDatabases = client.ListDatabases().ToList();
+            return MongoDBAccess.getEstudianteDocuments().Find(new BsonDocument()).ToList();
+        }
 
-                return allDatabases;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
+        //https://localhost:44370/api/Estudiante?carnet=12345&nombre="Andres A"&correo="aalopz02@gmial.com"&telefono=8888&contrasenna="clave"
+        public void PostEstudiante(int carnet, String nombre, String correo, int telefono, String contrasenna) {
+
+            var document = new BsonDocument { 
+                { "Carnet", carnet },
+                { "Nombre", nombre },
+                { "Correo", correo },
+                { "Telefono", telefono },
+                { "Contrasenna", Encript.EncriptString(contrasenna) }
+            };
+            MongoDBAccess.getEstudianteDocuments().InsertOne(document);
         }
     }
 }
---
+++ ApiMongo/Mist/MongoDBAccess.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiMongo.Controllers
+{
+    public class MongoDBAccess
+    {
+        static MongoClient client = new MongoClient("mongodb://localhost:27017");
+        static IMongoDatabase database = client.GetDatabase("xtecDB");
+
+        public MongoDBAccess() {
+        }
+
+        public static IMongoCollection<BsonDocument> getEstudianteDocuments() {
+
+            return database.GetCollection<BsonDocument>("Estudiantes");
+        }
+    }
+}
