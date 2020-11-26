a596edadf20bbc7d6e3f5c792b40f2cba785d296
aalopz02 <aalopz02@gmail.com>
2020-11-25 20:13:01 -0600

    se agrega conexion con mongo

---
+++ ApiMongo/Controllers/EstudianteController.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ApiMongo.Controllers
+{
+    public class EstudianteController : ApiController
+    {
+        // GET: Estudiante
+        public List<BsonDocument> Get()
+        {
+            try
+            {
+                var connString = "mongodb://localhost:27017";
+                MongoClient client = new MongoClient(connString);
+                var allDatabases = client.ListDatabases().ToList();
+
+                return allDatabases;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
