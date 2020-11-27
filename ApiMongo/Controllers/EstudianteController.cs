4cc36e1b76870893aa7e30226c9500ff1933eac0
aalopz02 <aalopz02@gmail.com>
2020-11-27 14:16:41 -0600

    get estudiante y patch listo

--- ApiMongo/Controllers/EstudianteController.cs
+++ ApiMongo/Controllers/EstudianteController.cs
@@ -12,13 +12,62 @@ namespace ApiMongo.Controllers
 {
     public class EstudianteController : ApiController
     {
-        // GET: Estudiante
+        // GET: Estudiantes
         public ICollection<BsonDocument> Get()
         {
             return MongoDBAccess.getEstudianteDocuments().Find(new BsonDocument()).ToList();
         }
 
-        //https://localhost:44370/api/Estudiante?carnet=12345&nombre="Andres A"&correo="aalopz02@gmial.com"&telefono=8888&contrasenna="clave"
+        // GET: Estudiante by carnet
+        public BsonDocument GetEstudiante(int carnet) {
+            var carnetQuery = new BsonDocument("Carnet", carnet);
+            foreach (BsonDocument estudiante in MongoDBAccess.getEstudianteDocuments().Find(new BsonDocument()).ToList()) {
+                BsonValue x;
+                estudiante.TryGetValue("Carnet", out x);
+                if (x.Equals(carnet)) {
+                    return estudiante;
+                }
+            }
+            return null;
+        }
+
+        //Patch estudiante
+        //https://localhost:44370/api/Estudiante?carnetEstudiante=12345&nombre=Andres_A&correo=aalopz02@gmial.com&telefono=8888&contrasenna=clave
+        public void PatchEstudiante(int carnetEstudiante, String nombre, String correo, int telefono, String contrasenna)
+        {
+            BsonDocument old = GetEstudiante(carnetEstudiante);
+            if (old == null)
+            {
+                return;
+            }
+            else {
+                BsonValue x;
+                old.TryGetValue("Carnet", out x);
+                BsonValue pass = contrasenna;
+                if (pass == null)
+                {
+                    old.TryGetValue("Contrasenna", out pass);
+                }
+                else {
+                    pass = Encript.EncriptString(contrasenna);
+                }
+                var document = new BsonDocument {
+
+                { "Carnet", int.Parse(x.ToString()) },
+                { "Nombre", nombre },
+                { "Correo", correo },
+                { "Telefono", telefono },
+                { "Contrasenna", pass }
+                };
+                BsonValue id;
+                old.TryGetValue("_id", out id);
+                MongoDBAccess.getEstudianteDocuments().UpdateOne(new BsonDocument("_id", id),new BsonDocument("$set", document));
+            }
+
+        }
+
+        //Post estudiante
+        //https://localhost:44370/api/Estudiante?carnet=12345&nombre=Andres_A&correo=aalopz02@gmial.com&telefono=8888&contrasenna=clave
         public void PostEstudiante(int carnet, String nombre, String correo, int telefono, String contrasenna) {
 
             var document = new BsonDocument { 
