80971a4192ee9850bf993d57a585ebd61bf5f160
aalopz02 <aalopz02@gmail.com>
2021-01-14 17:52:06 -0600

    mas pegas

---
+++ ApiSQL/Controllers/EVALUACIONController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApiSQL.Models;
+
+namespace ApiSQL.Controllers
+{
+    public class EVALUACIONController : ApiController
+    {
+        // GET: api/EVALUACION
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/EVALUACION/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/EVALUACION
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/EVALUACION/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/EVALUACION/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApiSQL/Models/DOCUMENTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiSQL.Models
+{
+    public class DOCUMENTO
+    {
+        public String Nombre { get; set; }
+        public String Data { get; set; }
+        public String Tamanno { get; set; }
+        public String Fecha_Subida { get; set; }
+        public String Carpeta_Nombre { get; set; }
+        public String Curso_Grupo { get; set; }
+        public String Curso_Codigo { get; set; }
+        public char Sem_Periodo { get; set; }
+        public String Sem_Anno { get; set; }
+
+        public DOCUMENTO(string nombre, string data, string tamanno, string fecha_Subida, string carpeta_Nombre, string curso_Grupo, string curso_Codigo, char sem_Periodo, string sem_Anno)
+        {
+            Nombre = nombre;
+            Data = data;
+            Tamanno = tamanno;
+            Fecha_Subida = fecha_Subida;
+            Carpeta_Nombre = carpeta_Nombre;
+            Curso_Grupo = curso_Grupo;
+            Curso_Codigo = curso_Codigo;
+            Sem_Periodo = sem_Periodo;
+            Sem_Anno = sem_Anno;
+        }
+    }
+}
